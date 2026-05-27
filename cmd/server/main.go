e76ecf0b291dcd6a88f8c9d06d7411f94e441492
Debarshee Chakraborty <debarsheechakraborty.11d@gmail.com>
2026-05-27 18:52:24 +0530

    add process entrypoint

---
+++ cmd/server/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/subscription-reconciler/internal/app"
+	"github.com/subscription-reconciler/internal/config"
+)
+
+func main() {
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatalf("load config: %v", err)
+	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	if err := app.Run(ctx, cfg); err != nil {
+		log.Fatalf("run app: %v", err)
+	}
+}
