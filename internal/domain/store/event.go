7d7acd2ff5368ed619fd4c416b99c9d7639cb7c7
Debarshee Chakraborty <debarsheechakraborty.11d@gmail.com>
2026-05-27 20:34:36 +0530

    add store event types

---
+++ internal/domain/store/event.go
@@ -0,0 +1,16 @@
+package store
+
+import "time"
+
+type EventType string
+
+const (
+	EventInitialPurchase EventType = "INITIAL_PURCHASE"
+	EventRenewal         EventType = "RENEWAL"
+	EventCancellation    EventType = "CANCELLATION"
+	EventBillingIssue    EventType = "BILLING_ISSUE"
+	EventExpiration      EventType = "EXPIRATION"
+	EventUnCancellation  EventType = "UN_CANCELLATION"
+)
+
+const SubscriptionDuration = 30 * 24 * time.Hour
