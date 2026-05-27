1d66efb0d50bf03842d72a1103ea73633338c962
Debarshee Chakraborty <debarsheechakraborty.11d@gmail.com>
2026-05-28 04:17:30 +0530

    register store route

--- internal/http/routes.go
+++ internal/http/routes.go
@@ -3,12 +3,17 @@ package http
 import (
 	"net/http"
 
+	"github.com/dinocodesx/subscription-reconciler/internal/http/handlers"
 	"github.com/dinocodesx/subscription-reconciler/internal/repository/postgres"
 )
 
 // NewRouter registers the assignment endpoints on Go's standard library mux.
 func NewRouter(store *postgres.Store) http.Handler {
 	mux := http.NewServeMux()
+
+	storeHandler := handlers.NewStoreWebhookHandler(store)
+
+	mux.HandleFunc("POST /webhooks/store", storeHandler.Handle)
 
 	return mux
 }
