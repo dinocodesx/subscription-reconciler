5160e6825353a8262913035d843f5ca1db544876
Debarshee Chakraborty <debarsheechakraborty.11d@gmail.com>
2026-05-28 09:08:05 +0530

    add shared worker loop

---
+++ internal/worker/loop.go
@@ -0,0 +1,27 @@
+package worker
+
+import (
+	"context"
+	"log"
+	"time"
+)
+
+func runLoop(ctx context.Context, logger *log.Logger, interval time.Duration, name string, run func(context.Context) error) {
+	if err := run(ctx); err != nil {
+		logger.Printf("%s initial run failed: %v", name, err)
+	}
+
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-ticker.C:
+			if err := run(ctx); err != nil {
+				logger.Printf("%s run failed: %v", name, err)
+			}
+		}
+	}
+}
