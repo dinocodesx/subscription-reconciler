3a0f45fd5884bab85f65ff1cae042a415a8eb49d
Debarshee Chakraborty <debarsheechakraborty.11d@gmail.com>
2026-05-28 06:17:45 +0530

    register mock carrier route

--- internal/http/routes.go
+++ internal/http/routes.go
@@ -1,7 +1,9 @@
 package http
 
 import (
+	"math/rand/v2"
 	"net/http"
+	"time"
 
 	"github.com/dinocodesx/subscription-reconciler/internal/http/handlers"
 	"github.com/dinocodesx/subscription-reconciler/internal/repository/postgres"
@@ -14,8 +16,12 @@ func NewRouter(store *postgres.Store) http.Handler {
 	storeHandler := handlers.NewStoreWebhookHandler(store)
 	marketplaceHandler := handlers.NewMarketplaceWebhookHandler(store)
 
+	mockCarrierHandler := handlers.NewMockCarrierHandler(rand.New(rand.NewPCG(uint64(time.Now().UnixNano()), 0)))
+
 	mux.HandleFunc("POST /webhooks/store", storeHandler.Handle)
 	mux.HandleFunc("POST /webhooks/marketplace/revoke", marketplaceHandler.Handle)
+
+	mux.HandleFunc("GET /mock/carrier/plan", mockCarrierHandler.Handle)
 
 	return mux
 }
