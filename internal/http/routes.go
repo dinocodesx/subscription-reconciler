43180c3d5ae53741bcfa43a888e79740c1aa77ce
Debarshee Chakraborty <debarsheechakraborty.11d@gmail.com>
2026-05-29 14:07:38 +0530

    added timeline routes

--- internal/http/routes.go
+++ internal/http/routes.go
@@ -16,12 +16,14 @@ func NewRouter(store *postgres.Store) http.Handler {
 	storeHandler := handlers.NewStoreWebhookHandler(store)
 	marketplaceHandler := handlers.NewMarketplaceWebhookHandler(store)
 	entitlementHandler := handlers.NewEntitlementHandler(store)
+	timelineHandler := handlers.NewTimelineHandler(store)
 
 	mockCarrierHandler := handlers.NewMockCarrierHandler(rand.New(rand.NewPCG(uint64(time.Now().UnixNano()), 0)))
 
 	mux.HandleFunc("POST /webhooks/store", storeHandler.Handle)
 	mux.HandleFunc("POST /webhooks/marketplace/revoke", marketplaceHandler.Handle)
 	mux.HandleFunc("GET /users/{id}/entitlement", entitlementHandler.Handle)
+	mux.HandleFunc("GET /users/{id}/timeline", timelineHandler.Handle)
 
 	mux.HandleFunc("GET /mock/carrier/plan", mockCarrierHandler.Handle)
 
